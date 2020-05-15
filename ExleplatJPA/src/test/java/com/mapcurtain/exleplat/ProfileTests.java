c90d0483157db8de259b039d596e2b6a0dd04ee3
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-15 17:21:50 -0600

    update db name for persistence, profile pulling correct first name

--- ExleplatJPA/src/test/java/com/mapcurtain/exleplat/ProfileTests.java
+++ ExleplatJPA/src/test/java/com/mapcurtain/exleplat/ProfileTests.java
@@ -1,6 +1,7 @@
 package com.mapcurtain.exleplat;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -44,7 +45,8 @@ class ProfileTests {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(profile);
+		assertEquals("jp", profile.getFirstName());
 	}
 
 }
