e4cb6aff1b4cced6ca9e6cbbfef123f96cb3f60a
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-15 17:18:26 -0600

    create profile test junit

---
+++ ExleplatJPA/src/test/java/com/mapcurtain/exleplat/ProfileTests.java
@@ -0,0 +1,50 @@
+package com.mapcurtain.exleplat;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.mapcurtain.exleplat.entities.Profile;
+
+class ProfileTests {
+	
+	private EntityManager em;
+	private static EntityManagerFactory emf;
+	private Profile profile;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("exleplat");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		profile = em.find(Profile.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		profile = null;
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
