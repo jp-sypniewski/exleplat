792b74a592bcd392298cac17c9c1729183caa0ad
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-16 16:12:43 -0600

    init spring starter project

---
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/ExleplatRestApplication.java
@@ -0,0 +1,13 @@
+package com.mapcurtain.exleplat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExleplatRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExleplatRestApplication.class, args);
+	}
+
+}
---
+++ ExleplatREST/src/test/java/com/mapcurtain/exleplat/ExleplatRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapcurtain.exleplat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExleplatRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
