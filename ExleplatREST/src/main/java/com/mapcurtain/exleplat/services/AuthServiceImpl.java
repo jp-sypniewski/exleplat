9a00b6dc549ca77e3877425a658e7d49154a77ea
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-20 16:42:00 -0600

    update rest build.gradle with security line, add password encoder bean to starter file and authsvcimpl

--- ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthServiceImpl.java
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthServiceImpl.java
@@ -1,5 +1,14 @@
 package com.mapcurtain.exleplat.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mapcurtain.exleplat.repositories.UserRepository;
+
+@Service
 public class AuthServiceImpl implements AuthService {
+	
+	@Autowired
+	private UserRepository userRepo;
 
 }
