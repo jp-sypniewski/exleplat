9a334fd6cb862165d766fcf15cd9d181a49dd971
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-20 16:57:22 -0600

    create register method with encoded password

--- ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthService.java
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthService.java
@@ -1,5 +1,9 @@
 package com.mapcurtain.exleplat.services;
 
+import com.mapcurtain.exleplat.entities.User;
+
 public interface AuthService {
+
+	User register(User user);
 
 }
--- ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthServiceImpl.java
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthServiceImpl.java
@@ -1,8 +1,10 @@
 package com.mapcurtain.exleplat.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.mapcurtain.exleplat.entities.User;
 import com.mapcurtain.exleplat.repositories.UserRepository;
 
 @Service
@@ -10,5 +12,16 @@ public class AuthServiceImpl implements AuthService {
 	
 	@Autowired
 	private UserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Override
+	public User register(User user) {
+		String encodedPW = encoder.encode(user.getPassword());
+		user.setPassword(encodedPW);
+		userRepo.saveAndFlush(user);
+		return user;
+	}
 
 }
