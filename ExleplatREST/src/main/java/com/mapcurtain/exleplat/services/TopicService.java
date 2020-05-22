7f8f98b716c5ec3e344a7a78ad015e31778e9ce2
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-22 11:34:29 -0600

    stub methods in topic service for endpoints in controller

--- ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/TopicService.java
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/TopicService.java
@@ -1,5 +1,17 @@
 package com.mapcurtain.exleplat.services;
 
+import java.util.List;
+
+import com.mapcurtain.exleplat.entities.Topic;
+
 public interface TopicService {
+
+	List<Topic> getAllTopics();
+
+	Topic getTopicById(int id);
+
+	Topic createNewTopic(Topic topic);
+
+	Topic updateTopic(int id, Topic topic);
 
 }
--- ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/TopicServiceImpl.java
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/TopicServiceImpl.java
@@ -1,8 +1,12 @@
 package com.mapcurtain.exleplat.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mapcurtain.exleplat.entities.Topic;
 import com.mapcurtain.exleplat.repositories.TopicRepository;
 
 @Service
@@ -10,5 +14,43 @@ public class TopicServiceImpl implements TopicService {
 	
 	@Autowired
 	private TopicRepository topicRepo;
+	
+	@Override
+	public List<Topic> getAllTopics(){
+		List<Topic> topics = topicRepo.findAll();
+		return topics;
+	}
+	
+	@Override
+	public Topic getTopicById(int id) {
+		Optional<Topic> opt = topicRepo.findById(id);
+		if (opt.isPresent()) {
+			Topic topic = opt.get();
+			return topic;
+		}
+		return null;
+	}
+	
+	@Override
+	public Topic createNewTopic(Topic topic) {
+		
+		// TODO add principal param, verify, add user/author to topic
+		
+		topic = topicRepo.saveAndFlush(topic);
+		return topic;
+	}
+	
+	@Override
+	public Topic updateTopic(int id, Topic topic) {
+		
+		// TODO add principal param, confirm it matches the topic already in the database
+		
+		Optional<Topic> opt = topicRepo.findById(id);
+		if (opt.isPresent()) {
+			Topic managedTopic = opt.get();
+			managedTopic.setTitle(topic.getTitle());
+		}
+		return null;
+	}
 
 }
