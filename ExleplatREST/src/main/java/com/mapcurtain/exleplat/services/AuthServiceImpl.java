f1f8907b8d48726330e0d9720e6299b7ffdc33b5
jp-sypniewski <jpsypniewski@icloud.com>
2020-05-22 11:46:03 -0600

    add profile persistence within user registration in auth svc

--- ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthServiceImpl.java
+++ ExleplatREST/src/main/java/com/mapcurtain/exleplat/services/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mapcurtain.exleplat.entities.User;
+import com.mapcurtain.exleplat.repositories.ProfileRepository;
 import com.mapcurtain.exleplat.repositories.UserRepository;
 
 @Service
@@ -14,12 +15,16 @@ public class AuthServiceImpl implements AuthService {
 	private UserRepository userRepo;
 	
 	@Autowired
+	private ProfileRepository profileRepo;
+	
+	@Autowired
 	private PasswordEncoder encoder;
 	
 	@Override
 	public User register(User user) {
 		String encodedPW = encoder.encode(user.getPassword());
 		user.setPassword(encodedPW);
+		profileRepo.saveAndFlush(user.getProfile());
 		userRepo.saveAndFlush(user);
 		return user;
 	}
